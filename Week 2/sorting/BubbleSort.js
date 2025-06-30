dfc854eb55a4e551bba37a473e7b3b7090d1e9b0
Abhiram N <abhiramnksd@gmail.com>
2025-06-30 21:04:43 +0530

    add more

--- Week 2/sorting/BubbleSort.js
+++ Week 2/sorting/BubbleSort.js
@@ -1,6 +1,6 @@
 function bubblesort(arr){
     for(let i=0;i<arr.length;i++){
-        for(let j=0;j<arr.length-i-1;j++){
+        for(let j=0;j<arr.length-i;j++){
             if(arr[j]>arr[j+1]){
                 [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
             }
@@ -9,6 +9,6 @@ function bubblesort(arr){
     return arr
 }
 
-let array=[61,56,18,22,46]
+let array=[61]
 console.log(bubblesort(array));
 

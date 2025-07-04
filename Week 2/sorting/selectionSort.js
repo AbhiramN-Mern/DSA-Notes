de1268c993ce1909819349fb6691ec64ff7955b2
Abhiram N <abhiramnksd@gmail.com>
2025-07-04 22:24:21 +0530

    hashTable

--- Week 2/sorting/selectionSort.js
+++ Week 2/sorting/selectionSort.js
@@ -1,16 +1,16 @@
-// function selectionsort(arr){
-//     for(let i=0;i<arr.length-1;i++){
-//         let min=i
-//         for(let j=i+1;j<arr.length;j++){
-//             if(arr[j]<arr[min]){
-//                 min=j
-//             }
-//         }
-//         if(i!=min){
-//             [arr[i],arr[min]]=[arr[min],arr[i]]
-//         }
-//     }
-//     return arr
-// }
-// let array=[10,40,30,60,28]
-// console.log(selectionsort(array))
+function selectionsort(arr){
+    for(let i=0;i<arr.length-1;i++){
+        let min=i
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[min]){
+                min=j
+            }
+        }
+        if(i!=min){
+            [arr[i],arr[min]]=[arr[min],arr[i]]
+        }
+    }
+    return arr
+}
+let array=[10,40,30,60,28]
+console.log(selectionsort(array))
